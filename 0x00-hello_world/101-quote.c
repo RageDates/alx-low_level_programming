0622af33712b03353525ed09590e2e66519c69a4
RageDates <ragesdate@example.com>
2023-03-16 13:38:14 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
