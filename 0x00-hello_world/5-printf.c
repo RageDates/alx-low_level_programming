2227944c0b48a1d79bf07b6f7f3337fcad567e59
RageDates <ragesdate@example.com>
2023-03-16 12:34:18 -0700

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a function that prints out a sting using printf
+ * return value of main is 0 if succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
