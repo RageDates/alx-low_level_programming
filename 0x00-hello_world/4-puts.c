c1f6adf96332fddbe97d85b36d9fcd69069f9d88
RageDates <ragesdate@example.com>
2023-03-16 12:14:23 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - A program that prints a string using puts
- */
+ * Return : 0 if successful
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
