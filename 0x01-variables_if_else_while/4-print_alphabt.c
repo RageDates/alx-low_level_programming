49fc4dcc3917bc06c0db03d8bd2c64f174568570
RageDates <ragesdate@example.com>
2023-03-17 15:43:20 -0700

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point for the function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z' ; ++i)
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
