6b8c834a9a06a7105753f4ad2475f4542bcb6509
RageDates <ragesdate@example.com>
2023-03-17 15:34:11 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point for the function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 'a'; i <= 2; ++i)
+	{
+		putchar(i);
+	}
+	for (j = 'A' ; j <= 'Z'; ++j)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
