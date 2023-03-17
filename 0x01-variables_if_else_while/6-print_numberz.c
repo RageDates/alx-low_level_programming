94b0ee795ad9ccfe9d130433d37c87097001d51e
RageDates <ragesdate@example.com>
2023-03-17 15:52:06 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point for the function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(48 + i);
+	putchar('\n');
+	return (0);
+}
