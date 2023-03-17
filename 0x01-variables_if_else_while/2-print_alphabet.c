4942f71f17eb224ca1bcbd865eac2bf5530fcbf6
RageDates <ragesdate@example.com>
2023-03-17 15:14:28 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point for the function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+	return (0);
+}
