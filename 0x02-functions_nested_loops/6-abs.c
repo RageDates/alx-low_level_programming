047453e9d078cf9e1b483d83e6c05dbfb30eff77
RageDates <ragesdate@example.com>
2023-03-21 12:15:56 -0700

    m

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints the alphabet
+ * @c: the number to be checked
+ *
+ * Return: 1 if true
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (-c);
+	}
+}
