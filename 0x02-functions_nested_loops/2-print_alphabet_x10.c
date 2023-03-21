ccf47ce45ed004d341753a43c5711af169b49a95
RageDates <ragesdate@example.com>
2023-03-21 10:40:02 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
-/**
- * main - entry point for the function
- * Return: 0 (success)
- */
-void print_alphabet_x10(void);
+#include "main.h"
 
-int main(void)
-{
-	print_alphabe_x10();
-	return (0);
-}
 /**
  * print_alphabet_x10 - prints the alphabet
  */
+
 void print_alphabet_x10(void)
 {
-	int i, j;
+	int j;
+	char i;
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 97; i <= 122; i++)
-			_putchar(48 + i);
-		_putchar(\n);
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
 	}
 }
