7c1b4caa32e5ebe4fcc484f7500cf524eee77338
RageDates <ragesdate@example.com>
2023-03-21 13:45:38 -0700

    m

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,25 @@
  */
 void print_times_table(int n)
 {
-	if (n < 15 && n > 0)
-		return (0);
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j == 0)
+					printf("%d", k);
+				else if (k < 10 && j != 0)
+					printf(",   %d", k);
+				else if (k >= 10 && k < 100)
+					printf(",  %d%d", k / 10, k % 10);
+				else if (k >= 100)
+					printf(", %d%d%d", k / 100, (k / 10) % 10, k % 10);
+			}
+			printf("\n");
+		}
+	}
 }
