94370914290d2930df59a4f2e9a8073f8ef7e4ca
RageDates <ragesdate@example.com>
2023-03-21 13:35:05 -0700

    m

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the alphabet
+ * @n: the number to be checked
+ *
+ */
+void print_times_table(int n)
+{
+	if (n < 15 && n > 0)
+		return (0);
+}
