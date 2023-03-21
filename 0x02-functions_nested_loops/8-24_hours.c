39dc550fb55fd57522903fa25b75e659956fbbc5
RageDates <ragesdate@example.com>
2023-03-21 12:35:03 -0700

    m

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the alphabet
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar(i);
+			_putchar(':');
+			if (j < 10)
+				_putchar('0');
+			_putchar(j);
+			_putchar('\n');
+		}
+	}
+}
