4cbbdae9a386b1113ce3cf91f7d63ac4116d9997
RageDates <ragesdate@example.com>
2023-03-21 13:14:08 -0700

    m

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - prints the times table
+ */
+void times_table(void)
+{
+	int i, j, l;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			l = i * j;
+			if (j == 0)
+				printf("%d", l);
+			if (l < 10 && j != 0)
+				printf(", %d", l);
+			else if (l >= 10)
+				printf(", %d%d", l / 10, l % 10);
+		}
+		printf("\n");
+	}
+}
