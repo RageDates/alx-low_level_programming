335a0eeb38d57bd04d97b83ae051b04f36fabab2
RageDates <ragesdate@example.com>
2023-03-21 13:24:30 -0700

    m

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints the alphabet
+ * @n: the number to be checked
+ *
+ */
+void print_to_98(int n)
+{
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d, ", i);
+	}
+}
