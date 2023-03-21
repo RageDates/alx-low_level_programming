e7f6aae7823482af12cea26d3817528a18685e14
RageDates <ragesdate@example.com>
2023-03-20 22:46:32 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point for the function
+ * Return: 0 (success)
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - prints the alphabet
+ */
+void print_alphabet(void)
+{
+	for (i = 97; i <= 122; i++)
+		_putchar(48 + i);
+	_putchar(\n);
+}
