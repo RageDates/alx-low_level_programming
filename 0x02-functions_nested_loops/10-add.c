0ac6e7416e68200cdcb3644c2f2a97b7b760806d
RageDates <ragesdate@example.com>
2023-03-21 13:19:47 -0700

    m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * add - adds two numbers
+ * @a: the number to be added
+ * @b: the number to be added
+ *
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
