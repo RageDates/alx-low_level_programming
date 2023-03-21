05c2f46121fd40942ca1eec68148ee0b101edc61
RageDates <ragesdate@example.com>
2023-03-21 12:18:48 -0700

    m

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the alphabet
+ * @c: the number to be checked
+ *
+ * Return: 1 if true
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
