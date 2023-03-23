6feacaaf8c2a46bb1d18aeb00c5b5d904bbe6521
RageDates <ragesdate@example.com>
2023-03-23 02:45:58 -0700

    m

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the alphabet
+ */
+void print_most_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		if (c != 2 && c != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
