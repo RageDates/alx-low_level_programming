ca4940b14b24823fbc94145f3ce479b8994ea064
RageDates <ragesdate@example.com>
2023-03-23 02:34:27 -0700

    m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - prints the alphabet
+ * @a: the number to be checked
+ * @b: the second number
+ *
+ * Return: 1 if true
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
