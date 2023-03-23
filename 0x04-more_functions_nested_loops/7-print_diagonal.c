0d6ec75ced386fb08f7fe2bc799232980398ae0c
RageDates <ragesdate@example.com>
2023-03-23 03:40:05 -0700

    m

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the alphabet
+ * @n: the number to be checked
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j, count = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < count; j++)
+				_putchar(' ');
+			count++;
+			_putchar('\\');
+		_putchar('\n');
+	}
+}
