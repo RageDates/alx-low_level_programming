009ffc5b9bbf605642bd0cfbcb43b7a368719111
RageDates <ragesdate@example.com>
2023-03-23 02:26:02 -0700

    m

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - prints the alphabet
+ * @c: the number to be checked
+ *
+ * Return: 1 if true
+ */
+int _isdigit(int c)
+{
+	if (c < 10 && c >= 0)
+		return (1);
+	else
+		return (0);
+}
+
