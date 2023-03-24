f6cc74ea3180de92bbf6311726c572f6e7e9851e
RageDates <ragesdate@example.com>
2023-03-24 02:08:47 -0700

    m

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i == 100)
+			printf("%d\n", i);
+		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i == 100)
-			printf("%d", i);
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
 	return (0);
 }
