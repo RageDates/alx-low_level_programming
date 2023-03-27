404e75e8cfc687a6d397ca4598e494ab6d33cb23
RageDates <ragesdate@example.com>
2023-03-27 16:20:27 -0700

    m

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,7 @@ void puts_half(char *str)
 {
 	int i, len = strlen(str);
 
-	if (len % 2 == 0)
-	{
-		for (i = 0; i < len / 2; i++)
-			printf("%c", str[i]);
-	}
-	else
-	{
-		for (i = (len - 1) / 2 + 1; i <= len; i++)
-			printf("%c", str[i]);
-	}
+	for (i = (len - 1) / 2 + 1; i < len; i++)
+		printf("%c", str[i]);
+	printf("\n");
 }
