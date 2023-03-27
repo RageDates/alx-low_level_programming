b1e35452eae0db0a28e30ccbf417ac54519ce078
RageDates <ragesdate@example.com>
2023-03-27 16:41:17 -0700

    m

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,10 @@ void print_array(int *a, int n)
 
 	if (n <= 0)
 		printf("\n");
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[n - 1]);
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", a[i]);
+		printf("%d\n", a[n - 1]);
+	}
 }
-
