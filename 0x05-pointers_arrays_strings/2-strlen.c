2db361b89ed70312847ed9faddb1d2075081b004
RageDates <ragesdate@example.com>
2023-03-27 14:53:23 -0700

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the alphabet
+ * @s: the number to be checked
+ *
+ * Return: the size of the string
+ */
+int _strlen(char *s)
+{
+	int size;
+
+	size = sizeof(*s) / sizeof(*s[0]);
+	return (size);
+}
