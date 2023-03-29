1466e0ed00efe76f11345d5f0966c9dce41ef3aa
RageDates <ragesdate@example.com>
2023-03-29 13:56:31 -0700

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: 1 if true
+ */
 int _strcmp(char *s1, char *s2)
+{
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+
+	if (len1 < len2)
+		return (-1);
+	if (len1 > len2)
+		return (1);
+	if (len1 == len2)
+		return (0);
+}
