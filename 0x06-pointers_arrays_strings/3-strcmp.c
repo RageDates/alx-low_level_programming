bd88740d2f5e5fcce3f4bb99611dd20cda233d20
RageDates <125882700+RageDates@users.noreply.github.com>
2023-03-30 20:52:49 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,20 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-	int i, count = 0;
+	int flag = 0, i = 0;
 
-	if (len1 < len2)
-		return (-1);
-	if (len1 > len2)
-		return (1);
-	if (len1 == len2)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		for (i = 0; i < len1; i++)
+		if (s1[i] != s2[2]
 		{
-			if (s1[i] == s2[i])
-				count++;
+			flag = 1;
+			break;
 		}
-		if (count == len1)
-			return (0);
-		else
-			return (1);
+		i++;
 	}
+	if (flag == 0)
+		return (0);
+	else
+		return (1);
+	
 }
