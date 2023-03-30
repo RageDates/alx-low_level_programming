8fd4c8a086206a8af6f431f761431a35d69e1106
RageDates <ragesdate@example.com>
2023-03-30 13:26:51 -0700

    m

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,16 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	int len = strlen(src);
 
-	if (n < len)
-	{
-		for (i = 0; i < n - 1; i++)
-			dest[i] = src[i];
-		dest[n - 1] = '\0';
-	}
-	else
-	{
-		for (i = 0; i <= len; i++)
-			dest[i] = src[i];
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
 	return (dest);
 }
