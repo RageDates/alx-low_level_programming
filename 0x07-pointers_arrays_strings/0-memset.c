0b48651e6f5e155d3a7c20be72fdeb2ce357ce82
RageDates <ragesdate@example.com>
2023-04-03 13:56:30 -0700

    m

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: The character to be filled
+ * @b: The constant byte
+ * @n: The number of bytes to fill
+ *
+ * Return: pointer to char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+}
