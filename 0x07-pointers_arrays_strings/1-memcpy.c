f7267386a314621dcf76731d8fb5e57cd117b9c6
RageDates <ragesdate@example.com>
2023-04-03 14:12:59 -0700

    m

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _memcpy - fills memory with a constant byte
+ * @dest: The character to be filled
+ * @src: The constant byte
+ * @n: The number of bytes to fill
+ *
+ * Return: pointer to char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int *i = dest;
+	int *j = src;
+
+	while (n--)
+		*j++ = *i++;
+	return (dest);
+}
