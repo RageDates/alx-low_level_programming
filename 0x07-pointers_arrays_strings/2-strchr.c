21752fc2b05ab29231ecd829cd99500c7209a136
RageDates <ragesdate@example.com>
2023-04-03 14:30:21 -0700

    m

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strchr - prints the alphabet
+ * @s: the char to be checked
+ * @c: the occurence
+ *
+ * Return: pointer to the 1st occurrence
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (*s + i);
+		i++;
+	}
+	return (NULL);
+}
+
