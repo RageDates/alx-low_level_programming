1dfb1520662c333a9a05ec938379c87306d54bfe
RageDates <ragesdate@example.com>
2023-04-05 15:28:16 -0700

    m

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * is_prime_number - check if n prime nb
+ * @n: the number to be checked
+ *
+ * Return: 1 (prime nb) else 0
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 0)
+		return (0);
+	return (1);
+}
