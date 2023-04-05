b5f914bb5e482e8f5b7b02a17e30285b7c3d3136
RageDates <ragesdate@example.com>
2023-04-05 15:03:53 -0700

    m

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - the natural square root of a nb
+ * @n: the number to be calculated
+ *
+ * Return: the natural square root of a nb
+ */
+int _sqrt_recursion(int n)
+{
+	if ( i*i != n)
+	return (-1);
+}
+
