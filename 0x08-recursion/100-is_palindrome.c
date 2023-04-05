23d9d0f7df6f1cb86b6284382ee81c2731b5f676
RageDates <ragesdate@example.com>
2023-04-05 15:54:25 -0700

    m

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * is_palindrome - prints the alphabet
+ * @s: the string to be checked
+ *
+ * Return: 1 if success else 0
+ */
+int is_palindrome(char *s)
+{
+	if (s != '\0')
+		return (1);
+	return (0);
+}
