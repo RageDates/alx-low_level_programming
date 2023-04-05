0004cb78db097efdf7fe69d2f3dc9554c353ab75
RageDates <ragesdate@example.com>
2023-04-05 13:26:54 -0700

    m

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *  - prints the alphabet
+ * @: the number to be checked
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
