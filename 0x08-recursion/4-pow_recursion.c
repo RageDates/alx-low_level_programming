987a25eefc8737804f4d97af89d64a751c752d6a
RageDates <ragesdate@example.com>
2023-04-05 14:49:31 -0700

    m

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _pow_recursion - value of x raised to the power of y
+ * @x: the number
+ * @y: the power
+ *
+ * Return: the value of x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y != 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
