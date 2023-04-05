d950002b6af581c1ff7439c3a4d3d62f7a748c59
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-05 21:10:12 +0000

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (!*s)
-	{
-		printf("\n");
-	}
-	else if (*s)
+	if (*s)
 	{
 		s++;
 		_print_rev_recursion(s);
 		printf("%c", *s);
 	}
+	else
+		printf("\n");
 }
