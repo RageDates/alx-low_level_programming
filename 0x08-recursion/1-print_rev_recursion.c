0d872383b40225fac2aacc21cb95e4af1e570453
RageDates <ragesdate@example.com>
2023-04-05 13:38:50 -0700

    m

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the char to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s != '\0')
+		printf("%c", _print_rev_recursion(*(s--));
+}
