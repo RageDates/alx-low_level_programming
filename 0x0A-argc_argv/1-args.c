fb23dfebff0a7ca8ebbddbf00cab693f06634acc
RageDates <ragesdate@example.com>
2023-04-06 02:50:08 -0700

    m

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point for the function
+ * @argc: The number of arguments
+ * @argv: An array of arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%c\n", argc);
+	return (0);
+}
