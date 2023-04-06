fed36e31d3a0e5c509c339c71f98a1620fa46b54
RageDates <ragesdate@example.com>
2023-04-06 02:56:33 -0700

    m

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point for the function
+ * @argc: The number of arguments
+ * @argv: An array of arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]);
+	return (0);
+}
