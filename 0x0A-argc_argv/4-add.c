09c8e20c1694d99a4a1a896a2c914de7d64b9752
RageDates <ragesdate@example.com>
2023-04-08 07:37:26 -0700

    m

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - entry point for the function
+ * @argc: The number of arguments
+ * @argv: An array of arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i]) != 0)
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	return (sum);
+}
