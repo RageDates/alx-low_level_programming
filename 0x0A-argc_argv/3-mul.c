623a971b117fcff1ee0c4b12a9541f619967d402
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-08 14:18:22 +0000

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
  *
  * Return: 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]);
 	return (0);
