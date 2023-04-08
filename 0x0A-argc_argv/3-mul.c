5e3b702eed7427c5e1cfa321f1458376cd4d1202
RageDates <ragesdate@example.com>
2023-04-08 07:24:34 -0700

    m

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,8 +8,13 @@
  *
  * Return: 0 (success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]);
 	return (0);
 }
