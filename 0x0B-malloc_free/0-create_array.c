0c812e31150cbc51cdc400c03b01aa3b6700a6f2
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-11 08:15:43 +0000

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int *ptr;
-	int i;
+	char *ptr;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 	ptr = (char *)malloc(size * sizeof(char));
-	if (ptr == 0)
+	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < (int)size; ++i)
-		ptr[i] = c[i];
+	for (i = 0; i < size; ++i)
+		ptr[i] = c;
 	return (ptr);
 }
