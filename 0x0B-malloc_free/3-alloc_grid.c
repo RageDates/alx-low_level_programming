0cda3308dc589cde78afe7bea3fda7de4fe34214
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-11 09:39:48 +0000

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,10 +17,10 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(height * sizeof(int *));
+	ptr = (int **)malloc(height * sizeof(int *));
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(width * sizeof(int));
+		ptr[i] = (int *)malloc(width * sizeof(int));
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
