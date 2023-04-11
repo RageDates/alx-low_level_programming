cf6bceb80ea86e964720c7509941cd2110a8ff34
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-11 09:24:55 +0000

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int length = strlen(s1) + strlen(s2) + 1;
 
 	if (s1 == NULL)
-		memset(s1, '\0', sizeof(strlen(s1)));
+		s1 = "";
 	if (s2 == NULL)
-		memset(s2, '\0', sizeof(strlen(s2)));
+		s2 = "";
 	ptr = (char *)malloc(length * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
