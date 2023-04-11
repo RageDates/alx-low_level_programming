00562a60f44f0cb1d7d6ee1c8e1faf1895695f40
RageDates <ragesdate@example.com>
2023-04-11 02:50:19 -0700

    m

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,23 +8,22 @@
  * @: the number to be checked
  */
 char* argstostr(int ac, char** av) {
-  if (ac == 0 || av == NULL) {
-        return NULL;
-    }
-    size_t len = 0;
-    for (int i = 0; i < ac; i++) {
-        len += strlen(av[i]) + 1; // +1 for the newline character
-    }
-    char* result = (char*) malloc(len * sizeof(char));
-    if (result == NULL) {
-        return NULL;
-    }
-    size_t pos = 0;
-    for (int i = 0; i < ac; i++) {
-        strcpy(result + pos, av[i]);
-        pos += strlen(av[i]);
-        result[pos++] = '\n';
-    }
-
-    return result;
+	if (ac == 0 || av == NULL) {
+		return NULL;
+	}
+	size_t len = 0;
+	for (int i = 0; i < ac; i++) {
+		len += strlen(av[i]) + 1;
+	}
+	char* result = (char*) malloc(len * sizeof(char));
+	if (result == NULL) {
+		return NULL;
+	}
+	size_t pos = 0;
+	for (int i = 0; i < ac; i++) {
+		strcpy(result + pos, av[i]);
+		pos += strlen(av[i]);
+		result[pos++] = '\n';
+	}
+	return result;
 }
