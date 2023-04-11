1da219a1c226dd284658419a7cdfb43f6fabdd92
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-11 09:55:13 +0000

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,19 +9,20 @@
  */
 char* argstostr(int ac, char** av)
 {
+	int i;
 	size_t length = 0, position = 0;
 	char *ptr;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (int i = 0; i < ac; i++) {
+	for (i = 0; i < ac; i++) {
 		length += strlen(av[i]) + 1;
 	}
 	ptr = (char*) malloc(length * sizeof(char));
 	if (ptr == NULL) {
 		return (NULL);
 	}
-	for (int i = 0; i < ac; i++) {
+	for (i = 0; i < ac; i++) {
 		strcpy(ptr + position, av[i]);
 		position += strlen(av[i]);
 		ptr[position++] = '\n';
