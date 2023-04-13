46ec7aad01b3e92b71a6a8fe795fa862dc8ec086
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-13 10:28:49 +0000

    Update 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  - prints the alphabet
+ * @: the number to be checked
+ */void *malloc_checked(unsigned int b)
+ {
+ 
