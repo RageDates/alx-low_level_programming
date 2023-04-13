179654e4ddedebe969f9b918caa0985620a3d7fb
RageDates <ragesdate@example.com>
2023-04-13 14:48:36 -0700

    m

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int is_valid_num(char* num) {
+    while(*num) {
+        if(!isdigit(*num)) {
+            return 0;
+        }
+        num++;
+    }
+    return 1;
+}
+
+int multiply(char* num1, char* num2) {
+    return atoi(num1) * atoi(num2);
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Error\n");
+        return 98;
+    }
+    char* num1 = argv[1];
+    char* num2 = argv[2];
+    if (!is_valid_num(num1) || !is_valid_num(num2)) {
+        printf("Error\n");
+        return 98;
+    }
+    int result = multiply(num1, num2);
+    printf("%d\n", result);
+    return 0;
+}
+
