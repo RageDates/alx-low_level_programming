461c996b0d8181aa218199c0d4cc6e6249df8c0c
RageDates <ragesdate@example.com>
2023-04-17 04:15:04 -0700

    m

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize a variable struct
+ * @d: pointer to struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
