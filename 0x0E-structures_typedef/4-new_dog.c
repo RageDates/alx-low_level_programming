9a3151ce8a25d892a056fc4c80d13cdfb953e7ce
RageDates <ragesdate@example.com>
2023-04-17 13:44:31 -0700

    m

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - create a new dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ *
+ * Return: The new struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *New = (dog_t *)malloc(sizeof(dog_t));
+
+	if (New == NULL)
+		return (NULL);
+	New->name = (char *)malloc(strlen(name) + 1);
+	if (New->name == NULL)
+	{
+		free(New);
+		return (NULL);
+	}
+	strcpy(New->name, name);
+	New->owner = (char *)malloc(strlen(owner) + 1);
+	if (New->owner == NULL)
+	{
+		free(New->owner);
+		free(New);
+		return (NULL);
+	}
+	strcpy(New->owner, owner);
+	New->age = age;
+	return (New);
+}
+
