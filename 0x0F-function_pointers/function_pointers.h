99475cd2b08b378b506ca6b15dcfaaf428510526
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-19 22:47:45 +0000

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,6 +5,17 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * op_func_t - Typedef for funct
+ */
+typedef int (*op_func_t)(int, int);
+/**
+ * op_func_t - Typedef for funct
+ */
+typedef struct {
+    char *op;
+    op_func_t func;
+} op_t;
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
