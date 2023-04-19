839db60acdff17c2153582ae117c2ea7f4607ce3
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-19 21:19:20 +0000

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -11,4 +10,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL & f != NULL)
 		f(name);
+	else
+		return;
 }
