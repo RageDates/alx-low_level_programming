87a8608e36669ab40f459d17dbd612d73544ce7a
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-19 23:07:57 +0000

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,11 +14,23 @@ int main(int argc, char *argv[])
 	void *main_addr = (void *)main;
 
 	if (argc != 2)
+	{
+		printf("Error\n");
 		exit(1);
+	}
 	if (num_bytes < 0)
+	{
+		printf("Error\n");
 		exit(2);
+	}
 	for (i = 0; i < num_bytes; i++)
-		printf("%02x", *((unsigned char *)main_addr + i));
-	printf("\n");
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx", *((unsigned char *)main_addr + i));
+	}
 	return (0);
 }
