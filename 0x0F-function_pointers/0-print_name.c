59e7cb2879e7db5271c5a2b5494b12458eeb6c19
RageDates <ragesdate@example.com>
2023-04-19 08:48:25 -0700

    m

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: The name to print
+ * @f: The function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
