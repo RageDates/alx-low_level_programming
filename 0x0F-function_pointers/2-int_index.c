83a7c35876bd0c537f89ab250df2f929942c9469
RageDates <ragesdate@example.com>
2023-04-19 14:48:20 -0700

    m

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index - search for an int
+ * @array: The array
+ * @size: The size of the array
+ * @cmp: pointer to compare function
+ *
+ * Return: index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+		else
+			return (-1);
+	}
+}
