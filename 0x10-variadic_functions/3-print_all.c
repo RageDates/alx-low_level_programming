79ad784c3371a2d6b1e4b49a45f1339c801bcfd1
RageDates <ragesdate@example.com>
2023-04-20 03:55:36 -0700

    m

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of parameters
+ */
+void print_all(const char * const format, ...)
+{
+	
