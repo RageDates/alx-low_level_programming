18bb7a6d25a9cecbaa00a9a9b9de6e50ce051fc3
RageDates <ragesdate@example.com>
2023-04-20 14:14:54 -0700

    m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,4 +8,43 @@
  */
 void print_all(const char * const format, ...)
 {
-	
+	va_list args;
+
+	va_start(args, format);
+
+	char c;
+	int i;
+	float f;
+	char *s;
+	int index = 0;
+
+	while (format[index] != '\0')
+	{
+		if (format[index] == 'c')
+		{
+			c = (char)va_arg(args, int);
+			printf("%c", c);
+		}
+		else if (format[index] == 'i')
+		{
+			i = va_arg(args, int);
+			printf("%d", i);
+		}
+		else if (format[index] == 'f')
+		{
+			f = (float)va_arg(args, double);
+			printf("%f", f);
+		}
+		else if (format[index] == 's')
+		{
+			s = va_arg(args, char*);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+		}
+		index++;
+	}
+	va_end(args);
+	printf("\n");
+}
