07eefd1dce0fbbe189a92d0bc3de7cc2df1eb52b
RageDates <ragesdate@example.com>
2023-04-20 03:44:04 -0700

    m

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints integers
+ * @n: numbers of parameters
+ * @separator: string between nbs
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_args(args, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
