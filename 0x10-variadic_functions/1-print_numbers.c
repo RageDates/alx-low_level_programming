7f5cbfb03ed18a30b5f47e21e002ec0a0a0f64fb
RageDates <ragesdate@example.com>
2023-04-20 03:45:42 -0700

    m

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,11 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
-		return (0);
+		return;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_args(args, int));
+		printf("%d", va_arg(args, int));
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
