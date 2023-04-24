a793bc37a278d4fb9e122b049e130e13f0435c34
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-24 20:11:18 +0000

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,30 +10,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int ARG = atoi(argv[1]);
-	int change;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
 	{
-		if (ARG < 0)
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			printf("0\n");
-			return (0);
-		}
-		else
-		{
-			change = (ARG / 25) +
-				(ARG % 25) / 10 +
-				((ARG % 25) % 10) / 5 +
-				(((ARG % 25) % 10) % 5) / 2 +
-				(((ARG % 25) % 10) % 5) % 2;
-			printf("%d\n", change);
+			result++;
+			num -= coins[j];
 		}
 	}
-	return (change);
+
+	printf("%d\n", result);
+	return (0);
 }
