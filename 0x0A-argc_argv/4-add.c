3d0142da84fc772807a5bd811314363fe9879ef4
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-24 20:09:04 +0000

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
 /**
  * main - entry point for the function
  * @argc: The number of arguments
@@ -9,25 +34,38 @@
  *
  * Return: 0 (success)
  */
-int main(int argc, char *argv[])
-{
-	int i, nb, sum = 0;
 
-	if (argc <= 1)
+int main(int argc, char *argv[])
+
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		nb = atoi(argv[i]);
-		if (nb > 0)
-			sum += nb;
+		if (check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
+
+		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+
+		count++;
 	}
-	return (sum);
+
+	printf("%d\n", sum); /*print sum*/
+
+	return (0);
 }
+
