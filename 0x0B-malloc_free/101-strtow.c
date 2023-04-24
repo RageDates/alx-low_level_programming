17ef6ef6cd56c4a7512dbd3cf96bf3b57221420a
RageDates <125882700+RageDates@users.noreply.github.com>
2023-04-24 20:22:54 +0000

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,58 +1,78 @@
-#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-#define MAX 256
+/**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
 
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
 char **strtow(char *str)
 {
-	unsigned long int i;
-	int j;
-	int num = 0;
-	char **words;
-	int x = 0;
-	char word[MAX];
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (str == NULL || strlen(str) == 0)
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
 		return (NULL);
-	for (i = 0; i < strlen(str); i++)
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-			num++;
-	}
-	words = (char **) malloc((num + 1) * sizeof(char *));
-	if (words == NULL)
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
-	for (i = 0; i < strlen(str); i++)
+
+	for (i = 0; i <= len; i++)
 	{
-		if (str[i] != ' ')
-			word[x++] = str[i];
-		else if (i > 0 && str[i - 1] != ' ')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			word[x] = '\0';
-			words[x / MAX] = strdup(word);
-			if (words[x / MAX] == NULL)
+			if (c)
 			{
-				for (j = 0; j < x / MAX; j++)
-					free(words[j]);
-				free(words);
-				return (NULL);
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
-			x = 0;
 		}
+		else if (c++ == 0)
+			start = i;
 	}
-	if (x > 0)
-	{
-		word[x] = '\0';
-		words[x / MAX] = strdup(word);
-		if (words[x / MAX] == NULL)
-		{
-			for (j = 0;j < x / MAX; j++)
-				free(words[j]);
-			free(words);
-			return (NULL);
-		}
-	}
-	words[num] = NULL;
-	return (words);
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
+
