ef61e5a44f51771cd2cbee4ea1486ba6f0623891
RageDates <ragesdate@example.com>
2023-04-27 12:35:38 -0700

    m

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - print the contents of a list
+ * @h: a struct list
+ *
+ * Return: the nomber of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+		{
+			h = h->next;
+			counter++;
+		}
+	}
+	return (counter);
+}
