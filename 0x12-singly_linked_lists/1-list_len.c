15a436339ca6f45ccc42eca53c739795065a2de6
RageDates <ragesdate@example.com>
2023-04-27 13:05:01 -0700

    m

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * list_len - print the contents of a list
+ * @h: a struct list
+ *
+ * Return: the nomber of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
