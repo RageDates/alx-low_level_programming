d12c0058bcc4a71878c0572819d19e2a3f5e166f
RageDates <125882700+RageDates@users.noreply.github.com>
2023-05-03 01:12:46 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - prints the number of elements in a listint_t list.
+ * listint_len - prints the number of elements in a listint_t
  * @h: Pointer to the head
  *
  * Return: The number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t node_count = 0;
+	size_t counter = 0;
 
 	while (h != NULL)
 	{
-		node_count++;
+		counter++;
 		h = h->next;
 	}
-	return (node_count);
+	return (counter);
 }
