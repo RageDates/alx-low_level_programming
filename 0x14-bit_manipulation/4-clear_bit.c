8bda78df48eb53d7f06c6a08b6fdafd628977223
RageDates <ragesdate@example.com>
2023-05-04 16:14:40 -0700

    m

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,6 +8,7 @@
  * Return: 1 if success else -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
+{
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	*n = (*n & ~(1UL << index));
