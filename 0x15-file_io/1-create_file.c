1ac8766102cdcbcb09081e7a805cd2e767921eac
RageDates <125882700+RageDates@users.noreply.github.com>
2023-05-09 00:08:45 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,19 +7,18 @@
 #include <unistd.h>
 
 /**
- * create_file - Creates a new file with the specified filename and writes the
- * specified text to the file.
+ * create_file - Creates a new file named filename
  *
- * @filename: The filename of the file to create.
- * @text_content: The text to write to the file. Can be NULL.
+ * @filename: the name of the file
+ * @text_content: the text to write to the file
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 if success, else -1
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	size_t len;
-	ssize_t bytes_written;
+	ssize_t Bwrite;
 
 	if (filename == NULL)
 		return (-1);
@@ -31,8 +30,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
-		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1)
+		Bwrite = write(fd, text_content, len);
+		if (Bwrite == -1)
 		{
 			close(fd);
 			return (-1);
