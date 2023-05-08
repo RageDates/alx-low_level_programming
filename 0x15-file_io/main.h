5e3bcda936e35b694ee8720bd5e8e806374034a2
RageDates <ragesdate@example.com>
2023-05-08 16:05:14 -0700

    m

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,7 @@ int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int check_elf_header(int fd);
+void print_elf_header(const char *filename);
 
 #endif
